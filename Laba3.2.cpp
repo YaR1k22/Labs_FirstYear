e9aa20fdb336125215cbd6434ca5f31397d14a79
Яр <74552342+YaR1k22@users.noreply.github.com>
2020-12-14 16:57:09 +0300

    Add files via upload

---
+++ Laba3.2.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+int i;
+int x;
+int c;
+int Fib(int x)
+{
+    if (x == 0)
+        return 0;
+    if (x == 1)
+        return 1;
+    return Fib(x - 1) + Fib(x - 2);
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    cout << "Введите число: " << endl;
+    cin >> x;
+    cout << "Последовательность Фибоначчи: ";
+    for (int c = 0; c < x; ++c)
+       cout << Fib(c) << " ";
+        return 0;
+}
